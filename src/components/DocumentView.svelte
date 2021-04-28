569f7e186e3f45ce0bc02d8e48efa8c969596e63
elis bergling <bergling.elis@gmail.com>
2021-04-28 11:07:08 +0200

    kan byta isEdit och isPrivate

--- src/components/DocumentView.svelte
+++ src/components/DocumentView.svelte
@@ -2,24 +2,41 @@
   import { currentDoc, path, docRef } from "../stores";
   import { db, auth } from "../firebase";
   import { Document } from "../models/document";
-  let text = $currentDoc.text;
-  let title = $currentDoc.title;
   let user = auth.currentUser;
+  let isEdit = false;
   function onSave() {
     db.doc(`users/${user.uid}/${$path}/${$currentDoc.id}/`).update({
       text: $currentDoc.text,
       title: $currentDoc.title,
+      isPrivate: $currentDoc.isPrivate,
     });
+  }
+
+  function toogleIsEdit() {
+    isEdit = !isEdit;
   }
 </script>
 
 <div id="root">
   {#if $currentDoc.id != "baba"}
-    <h1>Title: {$currentDoc.title}</h1>
-    <p>Text:</p>
-    <input type="text" placeholder="text" bind:value={$currentDoc.text} />
-    <p>Is Private: {$currentDoc.isPrivate}</p>
-    <input type="button" value="Save Doc" on:click={onSave} />
+    {#if isEdit}
+      <h1>Title:</h1>
+      <input type="text" placeholder="title" bind:value={$currentDoc.title} />
+      <p>Text:</p>
+      <input type="text" placeholder="text" bind:value={$currentDoc.text} />
+      <div id="isPrivate">
+        <input type="checkbox" bind:checked={$currentDoc.isPrivate} />
+        <p>Is Private: {$currentDoc.isPrivate}</p>
+      </div>
+
+      <input type="button" value="Save Doc" on:click={onSave} />
+      <input type="button" value="View Doc" on:click={toogleIsEdit} />
+    {:else}
+      <h1>Title: {$currentDoc.title}</h1>
+      <p>Text: {$currentDoc.text}</p>
+      <p>Is Private: {$currentDoc.isPrivate}</p>
+      <input type="button" value="Edit Doc" on:click={toogleIsEdit} />
+    {/if}
   {:else}
     <p>No DOCUEMNT is fuckING selected</p>
   {/if}
@@ -27,7 +44,7 @@
 
 <style>
   #root {
-    height: 80vh;
+    height: 50vh;
     width: 20rem;
     background-color: cornsilk;
     padding: 1rem;
@@ -37,5 +54,10 @@
   }
   p {
     font-size: medium;
+  }
+
+  #isPrivate {
+    flex-direction: row;
+    display: flex;
   }
 </style>
